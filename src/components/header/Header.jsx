ce215f6ba583bd3948cc9ce03534882228fd4783
Resti Aryani <restiaryani01@gmail.com>
2022-10-28 13:01:44 +0700

    personaltask

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div>Resti Todo List</div>
+    </div>
+  );
+}
+export default Header;
